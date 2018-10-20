cfc069c01ac302ee18581f93701529ffa1c50f13
Roelof Roos <github@roelof.io>
2018-10-20 21:46:13 +0200

    Add content reflow
    This joins WordPress contents (like paragraphs and videos) in one container
    element, instead of a .container div around each block.

--- gumbo-millennium/src/Hooks/PostConversionHook.php
+++ gumbo-millennium/src/Hooks/PostConversionHook.php
@@ -15,6 +15,13 @@ use Gumbo\Plugin\Shortcodes\Sponsor;
 class PostConversionHook extends AbstractHook
 {
     /**
+     * Class name of the containers containing WordPress content
+     *
+     * @var string
+     */
+    const REFLOW_CLASS_NAME = 'gumbo-normal-content';
+
+    /**
      * List of available shortcodes
      *
      * @var array
@@ -22,6 +29,26 @@ class PostConversionHook extends AbstractHook
     const SHORTCODES = [
         Sponsor::class
     ];
+
+    /**
+     * Returns if the given node is eligible to be reflowed into the first node.
+     *
+     * @param DOMNode $node
+     * @return bool
+     */
+    protected static function isReflowableContainer(\DOMNode $node) : bool
+    {
+        // Skip if no attributes
+        if (!$node->attributes) {
+            return false;
+        }
+
+        // Find class attribute
+        $nodeClass = $node->attributes->getNamedItem('class');
+
+        // Object is reflowable if it has a class and the class contains the reflow class name
+        return ($nodeClass && stripos($nodeClass->nodeValue, self::REFLOW_CLASS_NAME) !== false);
+    }
 
     /**
      * Bind to WordPress
@@ -98,29 +125,26 @@ class PostConversionHook extends AbstractHook
         // Get shortcodes
         $shortcodes = $this->getShortCodes($document);
 
-        // Get list of effective shortcodes
+        // Log comments as array, since we're deleting them
         $nodeDeletionQueue = [];
 
+        // Handle each comment
         foreach ($comments as $comment) {
+            // Add comment to deletion queue
+            $nodeDeletionQueue[] = $comment;
+
+            // Get value
             $value = $comment->nodeValue;
             $value = trim($value);
 
             // Skip non-Gutenberg shortcodes
-            if (!preg_match('/^(\/)?wp:gumbo\/([a-z0-9\-]+)\s*(\{.+\})?\s*\/?$/', $value, $matches)) {
+            if (!preg_match('/^wp:gumbo\/([a-z0-9\-]+)\s*(\{.+\})?\s*\/?$/', $value, $matches)) {
                 continue;
             }
 
             // Make sure all arguments are counted for
             while (count($matches) < 3) {
                 $matches[] = null;
-            }
-
-            // Flag the node for deletion
-            $nodeDeletionQueue[] = $comment;
-
-            // If the node is an end-node, it only needs to be flagged for removal
-            if ($matches[1] === '/') {
-                continue;
             }
 
             // Get name and arguments
@@ -132,16 +156,20 @@ class PostConversionHook extends AbstractHook
             }
         }
 
-        // Delete all comments
+        // Find and delete all comments
         foreach ($nodeDeletionQueue as $comment) {
             $comment->parentNode->removeChild($comment);
         }
 
-        $post->post_content_filtered = $document->saveHTML();
+        // Reflow content, after removing all comments
+        $this->reflow($document);
+
+        // Export HTML
+        $html = preg_replace('/^<\?xml encoding="utf-8"\s*\?>/i', '', $document->saveHTML());
 
         // Save filtered content
         $wpdb->update($wpdb->posts, [
-            'post_content_filtered' => $post->post_content_filtered
+            'post_content_filtered' => $html
         ], [
             'ID' => $post->ID
         ]);
@@ -177,5 +205,79 @@ class PostConversionHook extends AbstractHook
         }
 
         return $data;
+    }
+
+    /**
+     * Reflows content of the document, by joining images, paragraphs and other WordPress content.
+     * All adjacent `gumbo-normal-content` div nodes are merged into one node, causing that content to
+     * flow properly.
+     *
+     * @param DOMDocument $document
+     * @return void
+     */
+    protected function reflow(\DOMDocument &$document) : void
+    {
+        // Find all possible containers
+        $divNodes = $document->getElementsByTagName('div');
+
+        // Filter out all actual containers
+        $containerNodes = [];
+        foreach ($divNodes as $node) {
+            if (self::isReflowableContainer($node)) {
+                $containerNodes[] = $node;
+            }
+        }
+
+        // Keep a deletion queue
+        $nodeDeletionQueue = [];
+
+        // Iterate container divs
+        foreach ($containerNodes as $container) {
+            // Skip if this container is scheduled for deletion
+            if (in_array($container, $nodeDeletionQueue, true)) {
+                continue;
+            }
+
+            // Reflow if the nextnode is eligible
+            if (self::isReflowableContainer($container->nextSibling)) {
+                $this->handleSingleReflow($container, $container->nextSibling, $nodeDeletionQueue);
+            }
+
+            // Add classname
+            $classAttr = $container->attributes->getNamedItem('class');
+            $classAttr->nodeValue = "{$classAttr->nodeValue} container--raw-content";
+        }
+
+        // Delete all nodes that were queued
+        foreach ($nodeDeletionQueue as $node) {
+            $node->parentNode->removeChild($node);
+        }
+    }
+
+    /**
+     * Moves all nodes in $thisContainer into $firstContainer, and adds itself to the $removeQueue.
+     *
+     * @param DOMNode $firstContainer
+     * @param DOMNode $thisContainer
+     * @param array $removeQueue
+     * @return void
+     */
+    protected function handleSingleReflow(\DOMNode $firstContainer, \DOMNode $thisContainer, array &$removeQueue) : void
+    {
+        // Add all nodes
+        foreach ($thisContainer->childNodes as $node) {
+            $firstContainer->appendChild($node);
+        }
+
+        // Find next node
+        $nextContainer = $thisContainer->nextSibling;
+
+        // Reflow next node if this node is not the final node
+        if (self::isReflowableContainer($nextContainer)) {
+            $this->handleSingleReflow($firstContainer, $nextContainer, $removeQueue);
+        }
+
+        // Append this node to the removeQueue
+        $removeQueue[] = $thisContainer;
     }
 }
